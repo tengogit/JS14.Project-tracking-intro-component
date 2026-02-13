c4b49318bb5fcbdae20c3931870ef4ccbcd2b37d
TENGO <tengogithub@gmail.com>
2026-02-13 20:52:01 +0400

    check

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const openBtn = document.getElementById('open-btn')
 const closeBtn = document.getElementById('close-btn')
 const main = document.getElementById('main')
 const headerSpan=[...document.querySelectorAll('.header-span')]
+const body =document.body
 
 
 
@@ -11,6 +12,7 @@ function burger() {
     openBtn.classList.toggle('open-btn-hide')
     closeBtn.classList.toggle('close-btn-show')
     main.classList.toggle('main-blur')
+    body.classList.toggle('no-scroll')
 }
 
 openBtn.addEventListener('click', burger)
